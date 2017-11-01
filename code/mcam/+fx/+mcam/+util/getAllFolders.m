705c5a70158232e6f2afe1c4192e62c1c31ae775
Sylvain Lacaze <sylvain.lacaze@mathworks.co.uk>
2017-11-01 23:46:20 +0000

    Correcting a bug in the code coverage.

--- code/mcam/+fx/+mcam/+util/getAllFolders.m
+++ code/mcam/+fx/+mcam/+util/getAllFolders.m
@@ -1,4 +1,8 @@
 function allFolders = getAllFolders( root )
+    fx.mcam.util.mustBeValidPath( root );
+    assert( exist( root, 'dir' ) == 7,...
+        'MCAM:InvalidPath',...
+        'The folder "%s" does not exist.' );
     listing = dir( fullfile( root, '**' ) );
     listing = listing([listing.isdir]);
     rootListing = strcmp( {listing.name}, '.' );
@@ -6,5 +10,9 @@ function allFolders = getAllFolders( root )
     parentListing = strcmp( {listing.name}, '..' );
     listing(parentListing) = [];
     allFolders = arrayfun( @(listing) fullfile( listing.folder, listing.name ), listing,...
-        'UniformOutput', false );
+        'UniformOutput', false )';
+    allFolders = [...
+        {root},...
+        allFolders,...
+        ];
 end
